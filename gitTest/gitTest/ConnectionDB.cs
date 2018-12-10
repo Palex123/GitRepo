aff16297ccfcf7d6e8be124baad96380fa5310b5
Alex <palex_a2@hotmail.com>
2018-12-10 22:00:10 +0300

    Adding class responsible for creating database connections

---
+++ gitTest/gitTest/ConnectionDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace gitTest
+{
+    public static class ConnectionDB
+    {
+        public static string ConnectionString()
+        {
+            return ConnectionString("localhost");
+        }
+        public static string ConnectionString(string Server)
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder["Server"] = Server;
+            stringBuilder.IntegratedSecurity = true;
+            return stringBuilder.ConnectionString;
+        }
+
+        public static SqlConnection Connection()
+        {
+            return new SqlConnection(ConnectionString());
+        }
+    }
+}
